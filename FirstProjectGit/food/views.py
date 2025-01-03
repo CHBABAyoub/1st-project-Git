33d02dd384436ffdda245c94e72bd0ca72b8532b
CHBAB Ayoub <chbabensam@gmail.com>
2025-01-03 14:33:01 +0100

    1st view creation for app food

--- FirstProjectGit/food/views.py
+++ FirstProjectGit/food/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-    def Index (request):
+def Index (request):
        return HttpResponse ('hello world')
