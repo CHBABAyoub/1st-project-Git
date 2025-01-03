519b981a96020b6422b19fc53e67b9ad3b998674
CHBAB Ayoub <chbabensam@gmail.com>
2025-01-03 15:53:49 +0100

    2nd view creation

--- FirstProjectGit/food/views.py
+++ FirstProjectGit/food/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def Index (request):
        return HttpResponse ('hello world')
+def Item(request):
+       return HttpResponse ('how are you doing')
